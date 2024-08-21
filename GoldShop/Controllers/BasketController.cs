cf0846471533c95a435b672cecab9a8efb3f0075
Key1 <keyvan.arjmnd@gmail.com>
2024-08-21 12:54:52 +0330

    Last

--- GoldShop/Controllers/BasketController.cs
+++ GoldShop/Controllers/BasketController.cs
@@ -1,5 +1,6 @@
 ﻿using GoldShop.Application.Interfaces;
 using GoldShop.Domain.Entity.Product;
+using GoldShop.Domain.Entity.User;
 using GoldShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -36,14 +37,15 @@ public class BasketController : Controller
 
         return RedirectToAction("Index", "Home");
     }
-    
+
     public async Task<IActionResult> Basket()
     {
         ViewBag.Cats = await _work.GenericRepository<Category>().TableNoTracking.ToListAsync();
         var basketProducts = new List<CheckOutDto>();
         if (HttpContext.Session.GetString("basket") != null)
         {
-            var basketList = JsonConvert.DeserializeObject<List<BasketDto>>(HttpContext.Session.GetString("basket")).ToList();
+            var basketList = JsonConvert.DeserializeObject<List<BasketDto>>(HttpContext.Session.GetString("basket"))
+                .ToList();
             foreach (var i in basketList)
             {
                 var prod = await _work.GenericRepository<Product>().TableNoTracking.Include(x => x.Category)
@@ -69,15 +71,75 @@ public class BasketController : Controller
 
         return View();
     }
+
+    public async Task<IActionResult> DiscountCheck(string code)
+    {
+        var result = await _work.GenericRepository<DiscountCode>().TableNoTracking
+            .FirstOrDefaultAsync(x => x.Code == code && x.IsActive && x.Count > 0);
+        if (result is { Count: > 0 })
+        {
+            HttpContext.Session.SetString("discountCode", JsonConvert.SerializeObject(result));
+            return Ok();
+        }
+        else
+        {
+            throw new Exception();
+        }
+    }
+
+    public async Task<IActionResult> CheckOut()
+    {
+        ViewBag.PostMethod = await _work.GenericRepository<PostMethod>().TableNoTracking.ToListAsync();
+        ViewBag.State = await _work.GenericRepository<State>().TableNoTracking.ToListAsync();
+        var discount = new DiscountCode();
+        if (HttpContext.Session.GetString("discountCode") != null)
+        {
+            discount = JsonConvert.DeserializeObject<DiscountCode>(HttpContext.Session.GetString("discountCode"));
+        }
+
+        ViewBag.Discount = discount;
+        ViewBag.Cats = await _work.GenericRepository<Category>().TableNoTracking.ToListAsync();
+        var basketProducts = new List<CheckOutDto>();
+        if (HttpContext.Session.GetString("basket") != null)
+        {
+            var basketList = JsonConvert.DeserializeObject<List<BasketDto>>(HttpContext.Session.GetString("basket"))
+                .ToList();
+            foreach (var i in basketList)
+            {
+                var prod = await _work.GenericRepository<Product>().TableNoTracking.Include(x => x.Category)
+                    .Include(x => x.GoldPrice).FirstOrDefaultAsync(x => x.Id == i.Id);
+                basketProducts.Add(new CheckOutDto
+                {
+                    Size = i.Size,
+                    GoldPrice = prod.GoldPrice,
+                    Brand = prod.Brand,
+                    Desc = prod.Desc,
+                    Image = prod.Image,
+                    Id = prod.Id,
+                    WagesAmount = prod.WagesAmount,
+                    Weight = i.Weight,
+                    Name = prod.Name,
+                    WagesPercentage = prod.WagesPercentage
+                });
+            }
+        }
+
+        ViewBag.BasketProd = basketProducts;
+        ViewBag.Curency = await _work.GenericRepository<GoldPrice>().TableNoTracking.FirstOrDefaultAsync();
+
+        return View();
+    }
+
     public IActionResult RemoveInBasket(int id)
     {
         var basketlist = new List<BasketDto>();
         if (HttpContext.Session.GetString("basket") != null)
         {
-            basketlist = JsonConvert.DeserializeObject<List<BasketDto>>(HttpContext.Session.GetString("basket")).ToList();
+            basketlist = JsonConvert.DeserializeObject<List<BasketDto>>(HttpContext.Session.GetString("basket"))
+                .ToList();
         }
 
-        basketlist.Remove(basketlist.First(x=>x.Id==id));
+        basketlist.Remove(basketlist.First(x => x.Id == id));
         HttpContext.Session.SetString("basket", JsonConvert.SerializeObject(basketlist));
         return RedirectToAction("Index", "Home");
     }
---
+++ GoldShop/Views/Basket/CheckOut.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GoldShop.Views.Basket;
+
+public class CheckOut : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
